fc59ad306f609a3586d5f7ebbe6049945241bdde
dominikmeyer23111989-hash <dominikmeyer23111989@gmail.com>
2026-07-27 19:01:50 +0200

    Add files via upload

--- modules/spielplan_page.py
+++ modules/spielplan_page.py
@@ -9,25 +9,27 @@ def show():
     st.title("🎮 Vereins-Arcade & Spiele")
     st.write("Wähle einfach das gewünschte Spiel aus, um direkt loszulegen.")
     
-    # Tabs für die verschiedenen Spiele (wächst einfach mit, wenn neue hinzukommen)
-    tab_dart, tab_poker, tab_mensch_aerger_dich_nicht, tab_kniffel, tab_bundesliga_tippspiel = st.tabs(["🎯 Vereins-Dart", "🃏 Vereins-Poker", "👨‍👩‍👧‍👦 Mensch ärgere dich nicht", "🎲 Kniffel", "⚽ Bundesliga-Tippspiel"])
+    # Der Tab-Variable einen anderen Namen geben, um den Konflikt zu lösen
+    tab_dart, tab_poker, tab_mensch_aerger_dich_nicht, tab_kniffel, tab_tipp = st.tabs([
+        "🎯 Vereins-Dart", 
+        "🃏 Vereins-Poker", 
+        "👨‍👩‍👧‍👦 Mensch ärgere dich nicht", 
+        "🎲 Kniffel", 
+        "⚽ Bundesliga-Tippspiel"
+    ])
     
     with tab_dart:
-        # Hier wird der komplette Dart-Automat eingebunden
         darts_page.show()
         
     with tab_poker:
-        # Hier wird der komplette Poker-Tisch eingebunden
         poker.show()
         
     with tab_mensch_aerger_dich_nicht:
-        # Hier wird das komplette Mensch ärgere dich nicht-Spiel eingebunden
         mensch_aerger_dich_nicht.show()
 
     with tab_kniffel:
-        # Hier wird das komplette Kniffel-Spiel eingebunden
         kniffel.show()
         
-    with tab_bundesliga_tippspiel:
-        # Hier wird das komplette Bundesliga-Tippspiel eingebunden
+    with tab_tipp:
+        # Hier wird nun das Modul korrekt angesprochen
         bundesliga_tippspiel.show()
